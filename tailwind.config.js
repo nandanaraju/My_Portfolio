82788d440f54c97e6c8e2d6137e26d7d488e2b5a
nandanaraju <nandanav0074@gmail.com>
2024-12-15 23:07:53 +0530

    modi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Adjust this to your project structure
-  theme: {
+export default {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'], // Adjust this to your project structure
+    theme: {
       extend: {
-          keyframes: {
-              slideIn: {
-                  '0%': { transform: 'translateY(-100%)', opacity: 0 },
-                  '100%': { transform: 'translateY(0)', opacity: 1 },
-              },
+        keyframes: {
+          slideIn: {
+            '0%': { transform: 'translateY(-100%)', opacity: 0 },
+            '100%': { transform: 'translateY(0)', opacity: 1 },
           },
-          animation: {
-              'slide-in': 'slideIn 1s ease-out',
-          },
+        },
+        animation: {
+          'slide-in': 'slideIn 1s ease-out',
+        },
       },
-  },
-  plugins: [],
-};
+    },
+    plugins: [],
+  };
+  
